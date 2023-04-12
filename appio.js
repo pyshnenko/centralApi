f350d2a8d643a65608112314c70b2d1e961e6b49
pyshnenko <pyshnenko94@yandex.ru>
2023-04-12 16:16:46 +0300

    socket server

---
+++ appio.js
@@ -0,0 +1,42 @@
+const content = require('fs').readFileSync(__dirname + '/index.html', 'utf8')
+
+const httpServer = require('http').createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/html')
+  res.setHeader('Content-Length', Buffer.byteLength(content))
+  res.end(content)
+})
+
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: "https://test.spamigor.site",
+  },
+});
+
+io.on('connection', socket => {
+  console.log('Подключение установлено')
+
+  let counter = 0
+  setInterval(() => {
+    // отправляем данные клиенту
+    socket.emit('hello', ++counter);
+  }, 1000)
+
+  // получаем данные от клиента
+  socket.on('hi', data => {
+    console.log('hi', data);
+    socket.join(String(data));
+  });
+  socket.on('bye', data => {
+    console.log('bye', data);
+    socket.leave(String(data));
+  });
+  socket.on('edit', data => {
+    let buf = JSON.parse(data);
+    console.log(buf);
+    io.to(String(buf.id)).emit('edit', data);
+  });
+})
+
+httpServer.listen(8813, () => {
+  console.log('Перейдите на http://localhost:8813')
+})
